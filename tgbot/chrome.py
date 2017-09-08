d38cd9a36fbeb7b51204bc3b20b9464248dd168c
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-09-08 16:55:13 +0300

    Try with xvbf

--- tgbot/chrome.py
+++ tgbot/chrome.py
@@ -18,7 +18,7 @@ def get_chrome():
     chrome_options.binary_location = os.environ['GOOGLE_CHROME_BIN']
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     driver = webdriver.WebDriver(executable_path=os.environ['CHROMEDRIVER_PATH'],
                                  chrome_options=chrome_options)
 
