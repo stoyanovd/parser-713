0d1c5675977a9e9d09e175a38fb8068a77c211c2
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-09-08 15:36:25 +0300

    Try use custom browser

--- tgbot/worker.py
+++ tgbot/worker.py
@@ -2,31 +2,33 @@ from splinter import Browser
 
 from tgbot.chrome import get_chrome
 
+
 def run_browser():
     browser = get_chrome()
-    with Browser() as browser:
-        # Visit URL
-        url = "https://bankrot.fedresurs.ru/Messages.aspx"
-        browser.visit(url)
+    # with Browser() as browser:
+    
+    # Visit URL
+    url = "https://bankrot.fedresurs.ru/Messages.aspx"
+    browser.visit(url)
 
-        elem = browser.find_by_id('ctl00_cphBody_mdsMessageType_tbSelectedText')
-        elem.click()
+    elem = browser.find_by_id('ctl00_cphBody_mdsMessageType_tbSelectedText')
+    elem.click()
 
-        # browser.fill('q', 'splinter - python acceptance testing for web applications')
+    # browser.fill('q', 'splinter - python acceptance testing for web applications')
 
-        elem = browser.find_by_text('Сообщение о судебном акте')
-        elem.click()
+    elem = browser.find_by_text('Сообщение о судебном акте')
+    elem.click()
 
-        elem = browser.find_by_id('ctl00_cphBody_ibMessagesSearch')
-        elem.click()
+    elem = browser.find_by_id('ctl00_cphBody_ibMessagesSearch')
+    elem.click()
 
-        # elem = browser.find_by_id('ctl00_cphBody_lnkbtnExcelExport')
-        # elem.click()
+    # elem = browser.find_by_id('ctl00_cphBody_lnkbtnExcelExport')
+    # elem.click()
 
-        elem = browser.find_by_xpath('// *[ @ id = "ctl00_cphBody_gvMessages"] / tbody / tr[2]')
+    elem = browser.find_by_xpath('// *[ @ id = "ctl00_cphBody_gvMessages"] / tbody / tr[2]')
 
-        t = str(elem.__dict__)
+    t = str(elem.__dict__)
 
-        t = t[:3000]
+    t = t[:3000]
 
     return t
